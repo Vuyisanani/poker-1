405dda55aa75331c7e679a4398289db89838b2ae
Biyi Akinpelu <bileckme@gmail.com>
2019-02-02 04:13:03 +0200

    Adding JUnit tests

---
+++ test/PokerTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import junit.framework.*;
+import poker.*;
+
+/**
+ *
+ * @author hp
+ */
+public class PokerTest extends TestCase {
+    // assigning the values
+   protected void setUp(){
+      
+   }
+
+   // test method to add two values
+   public void testManualPlay(){
+      
+   }
+}
